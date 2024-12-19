0f5f17a4e1c940b4a7ec4dc6ecbfe58741e8f52c
Justin Franken <justin272@gmx.de>
2024-12-19 23:41:41 +0100

    wrote two functions to download revenues and compute growth rates.

--- code/real_data/downloading_real_data.R
+++ code/real_data/downloading_real_data.R
@@ -1,31 +1,100 @@
-# Load required libraries
-library(rvest)
-library(dplyr)
+# function to download real financial data from macrotrends.net
 
-# Define the URL
-url <- "https://macrotrends.net/stocks/charts/AAPL/apple/revenue"
 
-# Read the webpage
-webpage <- read_html(url)
+download_revenue_data <- function(ticker, corp_name){
+  #' Downloads quarterly real financial revenue data from listed equities from macrotrends.
+  #' 
+  #' Parameters:
+  #' - ticker (string): Ticker of the equity to be downloaded in upper case. E.g. 'AAPL'.
+  #' - corp_name (string): Corporate name in lower case of that company. E.g. 'apple'.
+  #' 
+  #' Returns:
+  #' - a data.frame containing 3 columns:
+  #'  - date: string of the format 'YYYY-MM-DD'.
+  #'  - revenue: Numerical revenues in mio USD.
+  #'  - TTM: Trailing 12 months, which shows the revenue of the past 12 months of the quarterly data.
+  
+  # get the data
+  url <- paste0(
+    "https://macrotrends.net/stocks/charts/", ticker, "/", corp_name, "/revenue"
+    )
+  URL <- GET(url,user_agent("Justin"))
+  webpage <- read_html(URL)
+  
+  temp <- webpage %>%
+    html_nodes(xpath = "//table[contains(@class, 'historical_data_table')]")
+  
+  quarterly_table <- temp[2] %>% 
+    html_table(fill = TRUE)
+  
+  # shape downloaded data
+  colnames(quarterly_table[[1]]) <- c(
+    "date", 
+    "revenue"
+    )
+  cleaned_data <- quarterly_table[[1]]
+  
+  cleaned_data$date <- as.Date(cleaned_data$date, format = "%Y-%m-%d")
+  cleaned_data$revenue <- as.numeric(
+    gsub("[$,]", "", cleaned_data$revenue)
+    )
+  cleaned_data <- cleaned_data[order(cleaned_data$date), ]
+  
+  # computing TTM
+  cleaned_data$TTM <- NA
+  for (i in 4:nrow(cleaned_data)) {
+    cleaned_data$TTM[i] <- sum(
+      cleaned_data$revenue[(i-3):i]
+      )
+  }
+  
+  out = as.data.frame(cleaned_data)
+  return(out)
+}
 
-# Extract the "Apple Quarterly Revenue" table
-tables <- webpage %>%
-  html_nodes(xpath = "//table[contains(@class, 'historical_data_table')]")
 
-# Select the second table (Apple Quarterly Revenue)
-quarterly_table <- tables[2] %>% html_table(fill = TRUE)
-
-# Clean and format the data
-cleaned_data <- table_data %>%
-  rename(Date = 1, Revenue = 2) %>%
-  mutate(
-    Revenue = gsub("[$,]", "", Revenue), # Remove dollar sign and commas
-    Revenue = as.numeric(Revenue),      # Convert to numeric
-    Date = as.Date(Date, format = "%Y-%m-%d") # Convert to Date format
+get_growth_rates <- function(input){
+  #' Compute the growth rates for quarterly revenue data
+  #' 
+  #' Parameters:
+  #' - input (data.frame): The output of download_revenue_data.
+  #' 
+  #' Returns:
+  #' - A data frame containing 5 columns with computed growth rates:
+  #'  - date: Column indicating the date.
+  #'  - quarterly_gr: Growth rate of raw quarterly data each quarter.
+  #'  - twelve_months_gr: Growth rate of quarterly data compared to previous 12 month revenue.
+  #'  - TTM_quarterly_gr: Growth rate of TTM data each quarter.
+  #'  - TTM_twelve_months_gr: Growth rate of TTM data compared to previous 12 month TTM revenue.
+  
+  # initiate matrix
+  growth_rates <- data.frame(
+    matrix(NA, dim(input)[1], 5)
+    )
+  colnames(growth_rates) <- c(
+    "date", 
+    "quarterly_gr", 
+    "twelve_months_gr", 
+    "TTM_quarterly_gr", 
+    "TTM_twelve_months_gr"
+    )
+  
+  # date
+  growth_rates[,1] <- input$date
+  
+  # quarterly data
+  growth_rates[,2] <- c(NA, diff(input[,2]) / input[-length(input$TTM), 2])
+  growth_rates[,3] <- c(
+    rep(NA, 4), 
+    (input[-(1:4), 2] - input[1:(nrow(input) - 4), 2])/input[1:(nrow(input) - 4), 2]
   )
-
-# View the cleaned data
-print(cleaned_data)
-
-# Optionally save the data
-write.csv(cleaned_data, "apple_quarterly_revenue.csv", row.names = FALSE)
+  
+  # TTM data
+  growth_rates[,4] <- c(NA, diff(input[,3]) / input[-length(input$TTM), 3])
+  growth_rates[,5] <- c(
+    rep(NA, 4), 
+    (input[-(1:4), 3] - input[1:(nrow(input) - 4), 3])/input[1:(nrow(input) - 4), 3]
+  )
+  
+  return(growth_rates)
+}
--- code/real_data/inspecting_real_data.R
+++ code/real_data/inspecting_real_data.R
@@ -1,7 +1,39 @@
-# Testing the behavior of real financial revenue growth for adjusting MC simulations.
+# testing the behavior of real financial revenue growth for adjusting MC simulations.
 
+# import sources
+source(paste0(getwd(),"/downloading_real_data.r"))
 
+# --- large cap pharmaceutical -------------------------------------------------
+# download growth rates
+eli_lilly <- get_growth_rates(download_revenue_data('LLY', 'eli-lilly'))
+novo_nordisk <- get_growth_rates(download_revenue_data('NVO', 'novo-nordisk'))
+johnson_johnson <- get_growth_rates(download_revenue_data('JNJ', 'johnson_johnson'))
+abbvie <- get_growth_rates(download_revenue_data('ABBV', 'abbvie'))
+merck <- get_growth_rates(download_revenue_data('MRK', 'merck'))
+astrazeneca <- get_growth_rates(download_revenue_data('AZN', 'astrazeneca'))
+novartis <- get_growth_rates(download_revenue_data('NVS', 'novartis-ag'))
+pfizer <- get_growth_rates(download_revenue_data('PFE', 'pfizer'))
 
+# merge growth rates
+merged_TTM_quarterly_gr = data.frame(
+  year = eli_lilly$date,
+  eli_lilly = eli_lilly$TTM_quarterly_gr,
+  novo_nordisk = novo_nordisk$TTM_quarterly_gr,
+  johnson_johnson = johnson_johnson$TTM_quarterly_gr,
+  merck = merck$TTM_quarterly_gr,
+  astrazeneca = astrazeneca$TTM_quarterly_gr,
+  novartis = novartis$TTM_quarterly_gr,
+  pfizer = pfizer$TTM_quarterly_gr
+)
 
-
-
+# plot growth rates
+growth_rates <- merged_TTM_quarterly_gr[, -1]
+matplot(
+  merged_TTM_quarterly_gr$year, growth_rates, type = "l", lty = 1, 
+  xlab = "Year", ylab = "Growth Rate", 
+  main = "Growth Rates of Companies Over Time"
+)
+legend(
+  "topright", legend = colnames(growth_rates), 
+  col = 1:ncol(growth_rates), lty = 1, cex = 0.8
+)
